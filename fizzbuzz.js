dfb0093ef6f92db9a83bc104591e29e49bb59ea6
Šarlote Kumpiņa <sarlote.kumpina@gmail.com>
2020-06-03 00:18:57 +0300

    adding multiple elements in a loop

--- fizzbuzz.js
+++ fizzbuzz.js
@@ -5,15 +5,40 @@ function main() {
     console.log("Running main");
     const myApp = document.getElementById("app");
     const myBoxes = document.getElementsByClassName("box");
-    styleEl(myApp, "200px", "60%", "rgb(150, 130, 110)");
-    styleEl(myBoxes[3] "100px", "200px", "rgb(70, 70, 110)");
+    styleEl(myApp, "100%", "60%", "rgb(150, 130, 110)");
+    styleEl(myBoxes[3], "100px", "200px", "rgb(70, 70, 110)");
+    styleEl(myBoxes[1], "50px", "100px", "rgb(90, 70, 90)", "I am a simple boxy");
+    addNewElement(myApp, "hr");
+    addNewElement(myApp, "p", "", [], "MY PARAGRAPH");
+    addNewElement(myApp, "p", "mySpceialPar", [], "MY PARAGRAPH whoo");
+    addNewElement(myApp, "p", "", ["round-corners", "green-bg"], "MY PARAGRAPH haa");
+
+    for (let i = 0; i < 100; i++) {
+        addNewElement(myApp, "div", "mySpceialPar", ["round-corners", "green-bg"], "ei ei");
+    }
 }
 
-function styleEl(myElement, height, width, bgColor) {
+function styleEl(myElement, height, width, bgColor, text = "") {
     myElement.style.height = height;
     myElement.style.width = width;
     myElement.style.backgroundColor = bgColor;
-}    
+    if (text !== "") {
+        myElement.innerText = text;
+    }
+}   
+
+function addNewElement(parent, tag, id = null, myClassList = [], text = "", styleObj = {}) {
+    const newEl = document.createElement(tag);
+    if (id !== "") {
+        newEl.id = id;
+    }
+    if (myClassList.length > 0) {
+        newEl.classList.add(...myClassList)
+    }
+    if (text !== "") {
+        newEl.innerText = text};
+    parent.appendChild(newEl);
+}
     
 //did not call this function
 function unusedFunction () {
